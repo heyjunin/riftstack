5b107088cb74e452e84c565e34e3372b04d631af
Dan Barclay <barclaysd@me.com>
2025-03-11 22:08:01 +0000

    chore: bump web deps

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { reactRouterDevTools } from 'react-router-devtools';
 import { defineConfig } from 'vite';
 import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -15,7 +14,6 @@ export default defineConfig({
         plugins: ['babel-plugin-react-compiler'],
       },
     }),
-    reactRouterDevTools(),
     reactRouter(),
     tsconfigPaths(),
   ],
